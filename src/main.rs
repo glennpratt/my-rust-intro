2eb369dac4848c490f6eae1d92c9241f95332ec4
Glenn Pratt <glenn.pratt@acquia.com>
2014-11-19 23:25:12 -0600

    Semver example.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate semver;
+
+use semver::Version;
+
+
 fn main() {
+    hello_world();
+    intro_semver();
+}
+
+fn hello_world() {
     println!("Hello, world!");
+}
+
+/// Couldn't call this semver because of the package name. Odd.
+fn intro_semver() {
+    assert!(Version::parse("1.2.3") == Ok(Version {
+        major: 1u,
+        minor: 2u,
+        patch: 3u,
+        pre: vec!(),
+        build: vec!(),
+    }));
+
+        println!("Versions compared successfully!");
 }
